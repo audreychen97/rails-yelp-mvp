3fb9c3ad6fbc72f13ee31cc67bb1115111ed4eb1
audrey chen <audreychen97@gmail.com>
2019-03-07 11:19:37 +0100

    updated yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  restaurants = Restaurant.create([
+    {name: "President Thai", address: "1 Thailand Way", phone_number: "6261234567", category: "chinese"},
+    {name: "In-N-Out", address: "1 California St", phone_number: "123456789", category: "italian"},
+    {name: "Chipotle", address: "1 Mexican Ave", phone_number: "0987654321",category: "japanese"},
+    {name: "Blenders", address: "1 Del Playa", phone_number: "3875966395", category: "french"},
+    {name: "Belgium Waffles", address: "1 Waffle Dr", phone_number: "marcomarco", category: "belgian"}
+  ])
